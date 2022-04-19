471785f38c86386d1bb0879b1702ab9387cb135d
arpangoswami4 <arpan.goswami21@gmail.com>
2022-04-19 12:45:35 +0530

    removes jquery

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
-$(document).ready(function() {
-    $(".icon i").click(function() {
-        $(".heading__topbar__navbar").toggleClass("responsive");
-        $(".heading").css("position", "relative"); 
-        $(".heading__topbar__title").toggleClass("p--left");
-        $(this).toggleClass("p--right");
-        $(".heading__topbar").toggleClass("margin--nil");
-        
-               
-        
-        
-    });
-    $(".heading__content__arrow").click(function(){
-        var w = $(window).width();
-        if (w <650) {      
-            $(".heading__content__text").toggle();
-            $(".heading__content__asset").toggle();
-            $(".heading__content__text").css("text-align", "center");
-        }
-        else{
-            $(".heading__content__placeholder").toggle();
-            $(".heading__content__text").toggle();
-            $(".heading__content__asset").toggle();
-        }
-    });
-    $(window).resize(function() {
-        if( $(this).width() >= 650 ) {
-            // code
-            $(".heading__content__asset").show();
-            $(".heading__content__text").show();
-            $(".heading__content__text").css("text-align", "justify");
-            $(".heading__content__placeholder").hide();
-        }
-        else{
-            $(".heading__content__text").hide();
 
+function mobileNav()
+{
+    document.getElementById("mob1").classList.toggle("responsive");
+    document.getElementById("mob4").style.position = "relative"; 
+    document.getElementById("mob2").classList.toggle("p--left");
+    document.getElementById("triple--dash").classList.toggle("p--right");
+    document.getElementById("mob3").classList.toggle("margin--nil");
+}
+
+var textContent=document.getElementById("bar1");
+var imgContent=document.getElementById("bar2");
+var holderContent=document.getElementById("bar3");
+
+
+function scroller(){
+
+    var w = window.outerWidth;
+    if (w <650) {      
+    
+        if(textContent.style.display=="none"){
+            textContent.style.display="flex";
+            imgContent.style.display="none";
+            textContent.style.textAlign = "center";
         }
-    });
+        else{
+            imgContent.style.display="flex";
+            textContent.style.display="none";
+        }
+        
+    
+    }
+    else{
+        if(textContent.style.display=="none" || imgContent.style.display=="none"){
+            textContent.style.display="flex";
+            imgContent.style.display="flex";
+            holderContent.style.display="none";
+        }
+        else{
+            textContent.style.display="none";
+            imgContent.style.display="none";
+            holderContent.style.display="flex";
+        }
+    }
+}
+window.onresize = resized;
+function resized(){
+    w=window.outerWidth;
+    if( w >= 650 ) {
+        imgContent.style.display="flex";
+        textContent.style.display="flex";
+        textContent.style.textAlign = "justify";
+        holderContent.style.display="none";
+    }
+    else{
+        textContent.style.display="none";
+        holderContent.style.display="none";
+        imgContent.style.display="flex";
+        
+
+    }
+    
+  }
     
 
-});
+
